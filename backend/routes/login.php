4c0cabf19cd71c365e88a0523e1e1f788c7bf671
Rahn Josue <rahnjosue83@gmail.com>
2025-04-25 19:05:58 -0300

    Contenido con nueva versión

---
+++ backend/routes/login.php
@@ -0,0 +1,23 @@
+<?php
+require_once './config/database.php';
+require_once 'usuario.php';
+
+$data = json_decode(file_get_contents("php://input"), true);
+$nombre = $data['nombre'];
+$email = $data['email'];
+$password = $data['password'];
+
+$usuario = new Usuario($nombre, $email, $password, $conexion);
+
+if ($usuario->login()) {
+  if ($usuario->registrar()) {
+    echo json_encode([
+      "message" => "Registro exitoso",
+      "redirect" => true,
+      "url" => "../../frontend/index.html" // redirigir al index
+    ]);
+  } echo json_encode([
+  "message" => "Credenciales incorrectas",
+  "redirect" => false
+]);
+}
